c6b5b1645e3105baa89819190a7345073453cccc
OxMarco <>
2023-11-27 19:16:15 +0100

    added payment capture

--- contracts/test/Integration.test.sol
+++ contracts/test/Integration.test.sol
@@ -9,6 +9,7 @@ import {Vault} from "../src/Vault.sol";
 import {MockRouter} from "../src/MockRouter.sol";
 
 contract IntegrationTest is PRBTest, StdCheats {
+    address public constant DEPLOYER = address(0x1);
     ERC20PresetMinterPauser public immutable token;
     MockRouter public immutable router;
     Accountant public immutable accountant;
@@ -17,13 +18,17 @@ contract IntegrationTest is PRBTest, StdCheats {
 
     constructor() {
         token = new ERC20PresetMinterPauser("test", "TEST");
+        vm.deal(DEPLOYER, 1 ether);
+
+        vm.startPrank(DEPLOYER);
         router = new MockRouter();
         accountant = new Accountant(address(router));
         vault1 = new Vault(0, address(accountant), 0, address(router));
-        vault2 = new Vault(0, address(accountant), 0, address(router));
+        vault2 = new Vault(1, address(accountant), 0, address(router));
 
         vault1.toggleTokenStatus(address(token));
         vault2.toggleTokenStatus(address(token));
+        vm.stopPrank();
     }
 
     function _deposit(Vault vault, uint256 amount) internal {
@@ -58,21 +63,20 @@ contract IntegrationTest is PRBTest, StdCheats {
         assertEq(token.balanceOf(address(this)), amount);
     }
 
-    function testDeposit() public {
-        uint256 amount1 = 1000;
-        uint256 amount2 = 2000;
+    function testDeposit(uint256 amount1, uint256 amount2) public {
+        vm.assume(amount2 < type(uint256).max - amount1);
 
         assertEq(accountant.balances(address(this), 0, address(token)), 0);
 
         _deposit(vault1, amount1);
         _deposit(vault2, amount2);
 
-        assertEq(accountant.balances(address(this), 0, address(token)), amount1 + amount2);
+        assertEq(accountant.balances(address(this), 0, address(token)), amount1);
+        assertEq(accountant.balances(address(this), 1, address(token)), amount2);
+        assertEq(accountant.checkBalance(address(this), address(token)), amount1 + amount2);
     }
 
-    function testWithdraw() public {
-        uint256 amount = 1000;
-
+    function testWithdraw(uint256 amount) public {
         _deposit(vault1, amount);
 
         vm.expectRevert();
@@ -81,9 +85,8 @@ contract IntegrationTest is PRBTest, StdCheats {
         _withdraw(vault1, amount);
     }
 
-    function testDepositWithdraw() public {
-        uint256 amount1 = 1000;
-        uint256 amount2 = 2000;
+    function testDepositWithdraw(uint256 amount1, uint256 amount2) public {
+        vm.assume(amount2 < type(uint256).max - amount1);
 
         _deposit(vault1, amount1);
         _deposit(vault2, amount2);
@@ -96,7 +99,9 @@ contract IntegrationTest is PRBTest, StdCheats {
         assertEq(accountant.checkBalance(address(this), address(token)), 0);
     }
 
-    function testDepositWithdrawWithFee() public {
+    function testDepositWithdrawWithFee(uint256 amount1, uint256 amount2) public {
+        vm.assume(amount2 < type(uint256).max - amount1);
+
         router.setFee(100);
         vm.deal(address(this), 1e18);
 
@@ -108,6 +113,56 @@ contract IntegrationTest is PRBTest, StdCheats {
         (success,) = address(accountant).call{value: 1000}("");
         assertTrue(success);
 
-        testDepositWithdraw();
+        testDepositWithdraw(amount1, amount2);
+    }
+
+    function testHold(uint256 amount1, uint256 amount2, uint256 amountOnHold) public {
+        vm.assume(amount2 < type(uint256).max - amount1);
+        vm.assume(amountOnHold <= amount1 + amount2);
+
+        _deposit(vault1, amount1);
+        _deposit(vault2, amount2);
+
+        vm.startPrank(DEPLOYER);
+        vm.expectRevert();
+        accountant.acquireHold(address(this), address(token), amount1 + amount2 + 1);
+        accountant.acquireHold(address(this), address(token), amountOnHold);
+        vm.stopPrank();
+
+        assertEq(accountant.checkBalance(address(this), address(token)), amount1 + amount2 - amountOnHold);
+
+        if (amountOnHold > 0) {
+            vm.expectRevert();
+            _withdraw(vault1, amount1);
+
+            vm.expectRevert();
+            _withdraw(vault2, amount2);
+        }
+
+        vm.prank(DEPLOYER);
+        accountant.releaseHold(address(this), address(token), amountOnHold);
+
+        _withdraw(vault1, amount1);
+        _withdraw(vault2, amount2);
+    }
+
+    function testCapture(uint256 amount, uint256 capture) public {
+        vm.assume(amount < type(uint256).max - capture);
+        vm.assume(capture <= amount);
+
+        _deposit(vault1, amount);
+
+        assertEq(token.balanceOf(DEPLOYER), 0);
+
+        vm.startPrank(DEPLOYER);
+        accountant.acquireHold(address(this), address(token), capture);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = capture;
+        address[] memory users = new address[](1);
+        users[0] = address(this);
+        accountant.capture(address(vault1), 0, address(token), users, amounts);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(DEPLOYER), capture);
     }
 }
